51cf485ac267bf7abc73b5723c0945ea34ac688a
Rohin Galhotra <jiminycracken@gmail.com>
2018-03-18 11:07:58 -0700

    added arg check and function calls to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
-#include "vect.h"
+#include "obj.h"
 
 int main(int argc, char** argv) {
 
+	obj* object = new obj();
+
+	if (argc < 4) {
+		std::cout << "usage: prog04 input output numIterations" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+
+	object->readData(argv[1]);
+
+	object->readData(argv[2]);
 	return 0;
 }
