5bb1bedd59b8475beae07c194050b13f05963a93
Rohin Galhotra <jiminycracken@gmail.com>
2018-03-18 10:44:57 -0700

    wrote readData function

--- src/obj.cpp
+++ src/obj.cpp
@@ -1,7 +1,40 @@
 #include "obj.h"
 
 void obj::readData(std::string filename) {
-
+	std::ifstream input(filename, std::ifstream::in);
+	std::string parse;
+	float temp[3];
+	char type = '\0';
+	if (!(input.is_open())) {
+		std::cout << "invalid filename" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	while (!input.eof()) {
+		if (getline(input, parse)) {
+			type = parse.at(0);
+			std::istringstream iss(parse);
+			switch (type) {
+				case '#':
+				break;
+				case 'v':
+					iss >> temp[0];
+					iss >> temp[1];
+					iss >> temp[2];
+					this->points.push_back(vect(temp[0], temp[1], temp[2]));
+				break;
+				case 'f':
+					iss >> temp[0];
+					iss >> temp[1];
+					iss >> temp[2];
+					this->faces.push_back(vect(temp[0], temp[1], temp[2]));
+				break;
+				default:
+					//white space
+				break;
+			}
+		}
+	}
+	input.close();
 }
 
 void obj::writeData(std::string filename) {
